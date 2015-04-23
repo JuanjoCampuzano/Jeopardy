466fb3c158c8054233a8a3ad2d6c160e5caa7eb6
Alejandro Sanchez <alejandro.s1993@gmail.com>
2015-04-23 12:22:45 -0500

    Created multiple screens

--- build/generated/src/org/apache/jsp/nueva_005fpista_jsp.java
+++ build/generated/src/org/apache/jsp/nueva_005fpista_jsp.java
@@ -4,7 +4,7 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.jsp.*;
 
-public final class nueva_005fcategoria_jsp extends org.apache.jasper.runtime.HttpJspBase
+public final class nueva_005fpista_jsp extends org.apache.jasper.runtime.HttpJspBase
     implements org.apache.jasper.runtime.JspSourceDependent {
 
   private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
@@ -46,31 +46,17 @@ public final class nueva_005fcategoria_jsp extends org.apache.jasper.runtime.Htt
       out.write("\n");
       out.write("<!DOCTYPE html>\n");
       out.write("<html>\n");
-      out.write("    <!--se incluye el header.jsp en donde se agregan todos los css y js-->\n");
-      out.write("    ");
-      out.write("\n");
       out.write("    <head>\n");
+      out.write("        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
+      out.write("        <title>Jeopardy</title>\n");
       out.write("        <style>\n");
-      out.write("            #categoria{\n");
-      out.write("                size:35px;\n");
-      out.write("                font-size: 50px;\n");
-      out.write("                width: 500px;\n");
-      out.write("                padding-top: 100px;\n");
-      out.write("                margin: 0px auto;\n");
-      out.write("            }\n");
-      out.write("            #align{\n");
-      out.write("                text-align: center;\n");
-      out.write("            }\n");
       out.write("            #bienvenida , #seleccion{\n");
       out.write("                color: #FFFFFF;\n");
       out.write("                font-family:Arial;\n");
       out.write("                font-size: 20px;\n");
       out.write("                text-align: center;\n");
       out.write("            }\n");
-      out.write("             body{\n");
-      out.write("                background: #060CE9;\n");
-      out.write("            }\n");
-      out.write("            \n");
+      out.write("\n");
       out.write("            #table_menu{\n");
       out.write("               margin: 0px auto;\n");
       out.write("               text-align: center;\n");
@@ -78,6 +64,9 @@ public final class nueva_005fcategoria_jsp extends org.apache.jasper.runtime.Htt
       out.write("            }\n");
       out.write("            td{\n");
       out.write("                padding: 30px;\n");
+      out.write("            }\n");
+      out.write("            body{\n");
+      out.write("                background: #060CE9;\n");
       out.write("            }\n");
       out.write("            #click_button{\n");
       out.write("                width:500px;\n");
@@ -90,29 +79,21 @@ public final class nueva_005fcategoria_jsp extends org.apache.jasper.runtime.Htt
       out.write("                text-shadow: 0 -1px 0px rgba(0, 0, 0, 0.3); \n");
       out.write("                color: #000000;\n");
       out.write("                border-radius: 10px; \n");
-      out.write("                \n");
       out.write("            }\n");
       out.write("        </style>\n");
       out.write("    </head>\n");
       out.write("    <body>\n");
-      out.write("        <!--input de la nueva categoria que quiere ingresar el usuario-->\n");
-      out.write("        <!--se agrega un div que alinea todos los elementos en el centro para \n");
-      out.write("        facilitar que los elementos se tengan que alinear uno por uno-->\n");
-      out.write("        <div id='align'>\n");
-      out.write("            <h1 id='bienvenida'>Bienvenido a creacion de categoria</h1>\n");
-      out.write("            <h2 id='seleccion'>Cual sera el nombre de tu nueva categoria?</h2>\n");
-      out.write("            <form>\n");
-      out.write("                <table id='table_menu'>\n");
-      out.write("                    <tr>\n");
-      out.write("                        <td><input type='text' id='categoria'/></td>\n");
-      out.write("                    </tr> \n");
-      out.write("                    <tr>\n");
-      out.write("                        <td><input id='click_button' type=\"submit\" value='Crear'/></td> \n");
-      out.write("                    </tr>\n");
-      out.write("                </table>\n");
-      out.write("            </form>\n");
-      out.write("        </div>\n");
-      out.write("        \n");
+      out.write("        <h1 id='bienvenida'>Creaci&oacute;n de pista</h1>\n");
+      out.write("        <h2 id='seleccion'>favor de seleccionar en la categoria y pista(s) que quieres agregar</h2>\n");
+      out.write("        <form>\n");
+      out.write("            <select>\n");
+      out.write("                <!--Llamar a poner la lista de la base de datos-->\n");
+      out.write("            </select>\n");
+      out.write("            <input type='number' id='cantidad'/>\n");
+      out.write("            <input type='submit' value='Generar'/>\n");
+      out.write("            <input type=\"text\" value=\"pista\"/>\n");
+      out.write("                <!--Generar la cantidad de inputs que puso en cantidad-->\n");
+      out.write("        </form>\n");
       out.write("    </body>\n");
       out.write("</html>\n");
     } catch (Throwable t) {

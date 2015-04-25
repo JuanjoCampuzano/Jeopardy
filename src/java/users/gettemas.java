26bef9990d06a7bb50048e077a4bbbbc110837bf
Alfredo Altamirano <fredy.altamirano8@gmail.com>
2015-04-25 12:22:15 -0500

    get classes as xml

--- src/java/users/gettemas.java
+++ src/java/users/gettemas.java
@@ -1,34 +1,25 @@
+package users;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package users;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
  *
  * @author alfredo_altamirano
  */
-@WebServlet(name = "getclases", urlPatterns = {"/getclases"})
-public class getclases extends HttpServlet {
+@WebServlet(urlPatterns = {"/gettemas"})
+public class gettemas extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -43,33 +34,16 @@ public class getclases extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-
-            HttpSession session = request.getSession();
-
-            try {
-                // Realizar conexion a la base de datos y extraer datos de usuarios
-                Class.forName("com.mysql.jdbc.Driver").newInstance();
-                Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/jeopardy", "root", "");
-                Statement stmt = con.createStatement();
-                ResultSet rs = stmt.executeQuery("SELECT nombre FROM Clase");
-                List<String> classes = new ArrayList<>();
-                while (rs.next()) {
-                    classes.add(rs.getString(1));
-                }
-
-                session.setAttribute("clases", classes);
-                
-            } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException e) {
-                System.out.println(e);
-            }
-
-            
-            
-            response.setContentType("text/xml");
-            ServletContext sc = getServletContext();
-            RequestDispatcher rd = sc.getRequestDispatcher("/clases.jsp");
-            rd.forward(request, response);
-
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 3.2 Final//EN\">");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet gettemas</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet gettemas at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
         }
     }
 
